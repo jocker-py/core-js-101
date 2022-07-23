005fb380a1f247dd89e6a0e8220b53c4145323ec
jocker-py <buladzislau@gmail.com>
2022-07-23 13:45:22 +0300

    feat: add solutions for date-time

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(val) {
+  return new Date(val);
 }
 
 /**
@@ -34,8 +33,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(val) {
+  return new Date(val);
 }
 
 
@@ -53,8 +52,18 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if (year % 4 !== 0) {
+    return false;
+  }
+  if (year % 100 !== 0) {
+    return true;
+  }
+  if (year % 400 !== 0) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -73,8 +82,20 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(start, end) {
+  const formatDate = (date) => {
+    let HH = Math.floor(date / 3600000);
+    let mm = Math.floor((date - HH * 3600000) / 60000);
+    let ss = Math.floor((date - HH * 3600000 - mm * 60000) / 1000);
+    let sss = Math.floor((date - HH * 3600000 - mm * 60000 - ss * 1000));
+    if (HH < 10) HH = `0${HH}`;
+    if (mm < 10) mm = `0${mm}`;
+    if (ss < 10) ss = `0${ss}`;
+    if (sss < 10) sss = `0${sss}`;
+    if (sss < 100) sss = `0${sss}`;
+    return `${HH}:${mm}:${ss}.${sss}`;
+  };
+  return formatDate(end - start);
 }
 
 
@@ -94,8 +115,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const d = new Date(date);
+  const [hours, minutes] = [d.getUTCHours(), d.getUTCMinutes()];
+  let angle = Math.abs(0.5 * (60 * (hours % 12) - 11 * minutes));
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return (angle * Math.PI) / 180;
 }
 
 
